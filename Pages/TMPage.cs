4aef2f6723cc894fd0ff910fdc0ea739f18a563b
Shweta Belekar <belekarshwetagmail.com>
2025-04-17 17:13:33 +1200

    POM implementation, fluent wait implementation

---
+++ Pages/TMPage.cs
@@ -0,0 +1,110 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurnUpPortal.Utilities;
+
+namespace TurnUpPortal.Pages
+{
+    public class TMPage
+    {
+        public void CreateTimeRecord(IWebDriver driver)
+        {
+            // create a Time record
+            IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+            createNewButton.Click();
+
+            IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+            typeCodeDropdown.Click();
+            Thread.Sleep(1000);
+
+            IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+            timeOption.Click();
+
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("ABC");
+
+            IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+            descriptionTextbox.SendKeys("ABCDescription");
+            Thread.Sleep(1000);
+
+            IWebElement priceTagOverlap = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+            priceTagOverlap.Click();
+
+            IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+            priceTextbox.SendKeys("20");
+
+            Wait.WaitToBeClickable(driver, "Id", "SaveButton", 3);
+
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(1000);
+
+            IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+            goToLastPageButton.Click();
+
+
+
+            IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+            if (newCode.Text == "ABC")
+            {
+                Console.WriteLine("Time record created successfully!");
+            }
+            else
+            {
+                Console.WriteLine("New time record has not been created!");
+            }
+
+        }
+
+        public void EditTimeRecord(IWebDriver driver)
+        {
+            // Create an Edit Record 
+            IWebElement editButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[1]/td[5]/a[1]"));
+            editButton.Click(); 
+            IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+            descriptionTextbox.Clear();
+            descriptionTextbox.SendKeys("cbaDescription");
+
+            /*IWebElement global_typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+            global_typeCodeDropdown.Click();*/
+
+            /*IWebElement typeCodeList = driver.FindElement(By.Id("TypeCode-list"));
+            typeCodeList.Click();*/
+
+
+
+
+
+            /*IWebElement global_timeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+            global_timeOption.Click();*/
+
+
+            /*IWebElement materialOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_option_selected\"]"));
+            materialOption.Click();*/
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(1000);
+        }
+        public void DeleteTimeRecord(IWebDriver driver)
+        {
+            // Create a Delete Record
+            IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+            goToLastPageButton.Click();
+            IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[2]/td[5]/a[2]"));
+            deleteButton.Click(); 
+            driver.SwitchTo().Alert().Accept();
+        }
+
+
+    }
+}
+
+
+
+
+
+
