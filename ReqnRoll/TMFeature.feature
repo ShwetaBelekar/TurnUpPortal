7aa852ed538076595c445e0d1e310ba35334bb30
Shweta Belekar <belekarshwetagmail.com>
2025-04-23 18:49:25 +1200

    ReqnRoll framework implemented and feature file created

---
+++ ReqnRoll/TMFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TMFeature
+As a Turnup portal admin user
+I would like to create, edit and delete time and material records
+So that I can manage employees time and materials successfully
+
+
+@regression
+Scenario: create time record with valid data
+	Given I logged into Turnup portal successfully
+	When I navigate to Time and Material page
+	When I create a time record
+	Then the record should be created successfully
---
+++ StepDefinition/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using Reqnroll;
+
+namespace TurnUpPortal.StepDefinition
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given("I logged into Turnup portal successfully")]
+        public void GivenILoggedIntoTurnupPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I navigate to Time and Material page")]
+        public void WhenINavigateToTimeAndMaterialPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I create a time record")]
+        public void WhenICreateATimeRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then("the record should be created successfully")]
+        public void ThenTheRecordShouldBeCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
